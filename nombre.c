6d78fb987ca97c36b0f46e7b00cf2004af47b36d
ubaldo264 <ubaldo-usg@hotmail.com>
2024-10-28 08:16:09 -0600

    Create nombre.c

---
+++ nombre.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char nombre[50]; 
+    
+		
+    printf("Introduce tu nombre: ");
+    scanf("%s", nombre);  
+    
+    printf("Mi nombre es: %s\n", nombre);
+
+    return 0;
+}
